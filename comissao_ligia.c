ade143c47ee92c1e1433e4dc9bc15748845737c8
experimecha <54911071+experimecha@users.noreply.github.com>
2019-09-04 17:14:05 -0300

    Update comissao_ligia.c

--- comissao_ligia.c
+++ comissao_ligia.c
@@ -15,53 +15,46 @@ int main(void)
     printf("Digite o valor da transacao:\n");
     scanf("%f", &valor_transacao);
 
-    if(valor_transacao <= 2500) //aqui voce estava colocando os numeros tipo assim "2.500", como costumamos escrever em portugues, porém em C o compilador entende que o ponto é referente à float, e por tbm isso seus resultados saíam esquisitos
-        comissao = (((valor_transacao)*(1.7/100)) + 30);
+    if (((valor_transacao)*(1.7/100) + 30)<=39)
     {
-        if (((valor_transacao)*(1.7/100))>9)
-        {
-            printf("Comissao de: %.2f\n", comissao);
-        }
-        else
-        {
-            printf("Comissao minima, de 39 reais");
-        }
+        printf("Comissao minima, de 39 reais");
+    }
+    else if (valor_transacao <= 2500) //aqui voce estava colocando os numeros tipo assim "2.500", como costumamos escrever em portugues, porém em C o compilador entende que o ponto é referente à float, e por tbm isso seus resultados saíam esquisitos
+    {
+        comissao = (((valor_transacao)*(1.7/100)) + 30);
+        printf("Comissao de: %.2f\n", comissao);
     }
 
-        if (valor_transacao >= 2501 && valor_transacao <= 6250)
-        {
-            comissao =(((valor_transacao)*(0.66/100)) + 56);
-            printf("Comissao de: %.2f\n", comissao);
-        }
+    else if (valor_transacao >= 2501 && valor_transacao <= 6250)
+    {
+        comissao =(((valor_transacao)*(0.66/100)) + 56);
+        printf("Comissao de: %.2f\n", comissao);
+    }
 
-        else if (valor_transacao >= 6251 && valor_transacao <= 20000)
-        {
-            (((valor_transacao)*(0.34/100)) + 76);
-            printf("Comissao de: %.2f\n", comissao);
+    else if (valor_transacao >= 6251 && valor_transacao <= 20000)
+    {
+        comissao=(((valor_transacao)*(0.34/100)) + 76);
+        printf("Comissao de: %.2f\n", comissao);
+    }
 
-        }
+    else if (valor_transacao >= 20001 && valor_transacao <= 50000)
+    {
+        comissao = (((valor_transacao)*(0.22/100)) + 100);
+        printf("Comissao de: %.2f\n", comissao);
+    }
 
-        else if (valor_transacao >= 20001 && valor_transacao <= 50000)
-        {
-            comissao = (((valor_transacao)*(0.22/100)) + 100);
-            printf("Comissao de: %.2f\n", comissao);
-        }
+    else if (valor_transacao >= 50001 && valor_transacao <= 500000)
+    {
+        comissao = (((valor_transacao)*(0.11/100)) + 155);
+        printf("Comissao de: %.2f\n", comissao);
+    }
 
-        else if (valor_transacao >= 50001 && valor_transacao <= 500000)
-        {
-            comissao = (((valor_transacao)*(0.11/100)) + 155);
-            printf("Comissao de: %.2f\n", comissao);
-        }
-
-
-        else if (valor_transacao > 500000) // descobri que nao pode se colocar um 'else' após um 'else if'. o 'else' só pode aparecer após um 'if'.
-        {
-            comissao =(((valor_transacao)*(0.09/100)) + 255);
-            printf("Comissao de: %.2f\n", comissao);
-        }
+    else if (valor_transacao > 500000) // descobri que nao pode se colocar um 'else' após um 'else if'. o 'else' só pode aparecer após um 'if'.
+    {
+        comissao =(((valor_transacao)*(0.09/100)) + 255);
+        printf("Comissao de: %.2f\n", comissao);
+    }
 
     return 0;
-
-
 }
 
